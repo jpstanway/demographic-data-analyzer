eb88543f97250f98d34ab4b5f7f853e2cfd59660
Jordan S <jpstanway@gmail.com>
2022-02-14 22:50:24 -0800

    calculate percentage of bachelors

--- demographic_data_analyzer.py
+++ demographic_data_analyzer.py
@@ -3,23 +3,34 @@ import pandas as pd
 
 def calculate_demographic_data(print_data=True):
     # Read data from file
-    df = None
+    df = pd.read_csv('adult.data.csv')
 
     # How many of each race are represented in this dataset? This should be a Pandas series with race names as the index labels.
-    race_count = None
+    race_count = df['race'].value_counts()
 
     # What is the average age of men?
-    average_age_men = None
+    average_age_men = df.loc[df['sex'] == 'Male', 'age'].mean().astype(int)
 
     # What is the percentage of people who have a Bachelor's degree?
-    percentage_bachelors = None
+    percentage_bachelors = (df.loc[df['education'] == 'Bachelors', 'education'].value_counts(
+    ) / len(df.index) * 100).astype(int)
 
     # What percentage of people with advanced education (`Bachelors`, `Masters`, or `Doctorate`) make more than 50K?
     # What percentage of people without advanced education make more than 50K?
 
     # with and without `Bachelors`, `Masters`, or `Doctorate`
-    higher_education = None
-    lower_education = None
+    higher_education = df.loc[
+        (df['education'] == 'Bachelors') |
+        (df['education'] == 'Masters') |
+        (df['education'] == 'Doctorate'),
+        'education'
+    ].value_counts()
+    lower_education = df.loc[
+        (df['education'] != 'Bachelors') &
+        (df['education'] != 'Masters') &
+        (df['education'] != 'Doctorate'),
+        'education'
+    ].value_counts()
 
     # percentage with salary >50K
     higher_education_rich = None
